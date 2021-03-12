c2882556ce55a428c0275e0b779f1571f0d000f5
Emizzari <emilie@tonnessen.com>
2021-03-12 08:43:36 +0100

    Adding CORS to the node server

--- server/index.js
+++ server/index.js
@@ -2,10 +2,13 @@ const express = require('express');
 const ships = require('./ships');
 const server = express();
 const port = 4000;
+const cors = require('cors');
 
 server.listen(port, () => {
   console.log(`Server listening at ${port}`);
 });
+
+server.use(cors());
 
 server.get(['/api/ships', '/api/ships/:query'], (req, res) => {
   const query = req.params.query;

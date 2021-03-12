390c7e8b2a9a7208bcc96816e0aee99e0a17941c
Emizzari <emilie@tonnessen.com>
2021-03-12 08:55:58 +0100

    Setting up Searchbar component with values and ts

--- src/components/search/SearchBar.tsx
+++ src/components/search/SearchBar.tsx
@@ -1,9 +1,25 @@
-import React from 'react'
+interface SearchBarProps {
+    search: () => void; // <-- Handler to not return anything, so I used void.
+    value: string;
+    clearInput: () => void; // <-- Handler to not return anything, so I used void.
+    iconType: string;
+}
 
-const SearchBar = () => {
+const SearchBar: React.FC<SearchBarProps> = ({ search, value, clearInput, iconType }) => {
     return (
-        <div>
-            This is the search bar
+        <div className="search">
+            <input 
+                type="text"
+                className="search__input"
+                placeholder="Search"
+                onChange={search}
+                value={value} />
+
+            <button>
+                <img 
+                    src={iconType}
+                    alt="Search Icon" />
+            </button>
         </div>
     )
 }
